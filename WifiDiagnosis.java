1e43612b12b5cc2529e65d813c4c912aebe1cfd4
Nathan Assefa <85200900+ProgrammingSmarts@users.noreply.github.com>
2022-01-26 04:58:52 -0500

    Fixed Formatting

--- WifiDiagnosis.java
+++ WifiDiagnosis.java
@@ -4,42 +4,49 @@ public class WifiDiagnosis {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
+
+		System.out.println("If you have a problem with internet connectivity, this Wifi"
+				+ " Diagnosis might work. \n\n\n" + "First Step: reboot your computer \n"
+				+ "Are you able to connect with the internet? (yes or no)");
 		
-		System.out.println("If you have a problem with internet connectivity, this Wifi"+
-	" Diagnosis might work. \n\n\n"+
-	"First Step: reboot your computer \n" + 
-	"Are you able to connect with the internet? (yes or no)");
 		String answer = s.next();
-		if(answer.compareTo("yes") == 0) {
+		
+		if (answer.compareTo("yes") == 0) {
 			System.out.println("rebooting your computer seemed to work");
 			System.exit(0);
+		
 		}
-		System.out.println("Second step: reboot your router \n"+
-		"Now are you able to connect with the internet? (yes or no)");
+		System.out.println(
+				"Second step: reboot your router \n" + "Now are you able to connect with the internet? (yes or no)");
 		answer = s.next();
-		if(answer.compareTo("yes") == 0) {
+		
+		if (answer.compareTo("yes") == 0) {
 			System.out.println("rebooting your router seemed to work");
 			System.exit(0);
 		}
-		System.out.println("Third step: make sure the cables to your router are " +
-		" plugged in firmly and your router is getting power");
+		
+		System.out.println("Third step: make sure the cables to your router are "
+				+ " plugged in firmly and your router is getting power");
+		
 		System.out.println("Now are you able to connect with the internet? (yes or no)");
 		answer = s.next();
-		if(answer.compareTo("yes") == 0) {
+		
+		if (answer.compareTo("yes") == 0) {
 			System.out.println("Checking the router's cables seemed to work");
 			System.exit(0);
-			}
+		}
+		
 		System.out.println("Fourth step: move your computer closer to your router");
 		System.out.println("Now are you able to connect with the internet? (yes or no)");
-				answer = s.next();
-				if(answer.compareTo("yes") == 0) {
-		System.out.println("Moving your computer closer to the router seemed to work");
-					System.exit(0);
-					}
-		System.out.println("Fifth step: contact your ISP \n"+
-			"Make sure your ISP is hooked up to your router.");
+		answer = s.next();
+		
+		if (answer.compareTo("yes") == 0) {
+			System.out.println("Moving your computer closer to the router seemed to work");
 			System.exit(0);
-			
+		}
+		
+		System.out.println("Fifth step: contact your ISP \n" + "Make sure your ISP is hooked up to your router.");
+		System.exit(0);
 
 	}
 
